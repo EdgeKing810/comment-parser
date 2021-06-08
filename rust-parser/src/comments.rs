78b46ad9090507155072fa14039f201ba143f1bc
Kishan Takoordyal <kt@esokia-webagency.com>
2021-06-08 17:34:11 +0400

    implement replies

--- rust-parser/src/comments.rs
+++ rust-parser/src/comments.rs
@@ -18,7 +18,8 @@ struct FirstStepComment {
     comment_id: String,
     comment: String,
     reply_to: String,
-    initial_replies: Vec<String>
+    initial_replies: Vec<String>,
+    final_replies: Vec<FirstStepComment>,
 }
 
 #[derive(Debug, Clone)]
@@ -33,7 +34,7 @@ impl FirstStepComments {
                 return Some(comment.clone());
             }
         }
-        
+
         None
     }
 
@@ -52,12 +53,13 @@ impl FirstStepComments {
                 comment_id: comment.comment_id.clone(),
                 comment: comment.comment.clone(),
                 reply_to: comment.reply_to.clone(),
-                initial_replies: replies
+                initial_replies: replies,
+                final_replies: vec![],
             });
         }
 
         FirstStepComments {
-            comments: final_comments
+            comments: final_comments,
         }
     }
 
@@ -85,12 +87,63 @@ impl FirstStepComments {
 
         depth
     }
+
+    fn construct_replies(&mut self) -> FirstStepComments {
+        let depth = self.get_depth();
+        let mut copied_comments = self.comments.clone();
+
+        for _n in 0..depth {
+            let mut final_comments = Vec::<FirstStepComment>::new();
+
+            for i in 0..(copied_comments.len()) {
+                let current_comment = &copied_comments[i];
+                let new_replies = &mut copied_comments[i].final_replies.clone();
+                let initial_replies = &mut copied_comments[i].initial_replies.clone();
+
+                for comm in copied_comments.iter() {
+                    if comm.initial_replies.len() == 0
+                        && current_comment
+                            .initial_replies
+                            .clone()
+                            .iter()
+                            .any(|r| *r == comm.comment_id)
+                    {
+                        let index = initial_replies
+                            .iter()
+                            .position(|x| *x == comm.comment_id)
+                            .unwrap();
+                        initial_replies.remove(index);
+                        new_replies.push(comm.clone());
+                    }
+                }
+
+                let new_comment = FirstStepComment {
+                    comment_id: current_comment.comment_id.clone(),
+                    comment: current_comment.comment.clone(),
+                    reply_to: current_comment.reply_to.clone(),
+                    initial_replies: initial_replies.to_vec(),
+                    final_replies: new_replies.to_vec(),
+                };
+
+                final_comments.push(new_comment);
+            }
+
+            copied_comments = final_comments;
+        }
+
+        FirstStepComments {
+            comments: copied_comments
+                .into_iter()
+                .filter(|c| c.reply_to.is_empty())
+                .collect::<Vec<FirstStepComment>>(),
+        }
+    }
 }
 
 pub fn parser(data: &str) -> Result<()> {
     let c: Comments = serde_json::from_str(data)?;
 
-    let final_comments = FirstStepComments::get_replies(c);
+    let final_comments = FirstStepComments::get_replies(c).construct_replies();
     let depth = final_comments.get_depth();
 
     println!("{:#?}", final_comments);
